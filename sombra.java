bc8fd3a4202166c3242fcc8a255d52efab07f4f0
tentale1 <jalvarez2153@universidadean.edu.co>
2019-08-19 17:10:14 -0500

    Actualizacion de autores

--- sombra.java
+++ sombra.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 /**
  * Programa para area sombreada de circulo
  * 
- * @author Carlos Alvarez 
+ * @author Sandra Martin, Israel Severiche, Jose Alvarez
  * @version 20190817
  */
 public class sombra
